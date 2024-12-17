04c6fa4e7cb6445867ce9fc4bafd2050512f2ef8
asrioth <liamswords@gmail.com>
2024-12-18 11:01:13 +1300

    added a method to identify block types from a block and tested it

--- src/test_textblockparser.py
+++ src/test_textblockparser.py
@@ -1,5 +1,5 @@
 import unittest
-from textblockparser import TextBlockParser
+from textblockparser import TextBlockParser, TextBlockType
 
 class TestTextBlockParser(unittest.TestCase):
     three_block_test_case = """# This is a heading
@@ -15,6 +15,13 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
     block3 = """* This is the first list item in a list block
 * This is a list item
 * This is another list item"""
+    block4 = "```this is code```"
+    block5 = """>This is the first quote item in a quote block
+>This is a quote item
+>This is another quote item"""
+    block6 = """1. This is the first ordered item in a ordered block
+2. This is an ordered item
+3. This is another ordered item"""
 
 
     def test_markdown_to_blocks(self):
@@ -39,6 +46,59 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         blocks = TextBlockParser.markdown_to_blocks(self.build_test_block([self.block1, self.block2, self.block3], 0, 0))
         expected_blocks = [self.block1 + self.block2 + self.block3]
         self.assertEqual(blocks, expected_blocks)
+
+    def test_block_to_block_type(self):
+        block_type = TextBlockParser.block_to_block_type(self.block1)
+        expected_block_type = TextBlockType.HEADING
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block2)
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block3)
+        expected_block_type = TextBlockType.UNORDERED_LIST
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block4)
+        expected_block_type = TextBlockType.CODE
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block5)
+        expected_block_type = TextBlockType.QUOTE
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block6)
+        expected_block_type = TextBlockType.ORDERED_LIST
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type("#####" + self.block1)
+        expected_block_type = TextBlockType.HEADING
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type("######" + self.block1)
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type("`" + self.block4)
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.block4 + "`")
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.mess_up_list(self.block3, "2. ", 2))
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.mess_up_list(self.block5, "* ", 1))
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.mess_up_list(self.block6, ">", 3))
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.mess_up_list(self.block6, "1. ", 3))
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+        block_type = TextBlockParser.block_to_block_type(self.mess_up_list(self.block6, "", 3))
+        expected_block_type = TextBlockType.PARAGRAPH
+        self.assertEqual(block_type, expected_block_type)
+
+        
+    def mess_up_list(self, block, replacement, start, line_to_change = 1):
+        lines = block.split("\n")
+        lines[line_to_change] = replacement + lines[line_to_change][start:]
+        return "\n".join(lines)
 
     def build_test_block(self, blocks, multi_new_line = 2, lead_trail_new_line = 0):
         block_case = "\n" * lead_trail_new_line + blocks.pop(0)
--- src/textblockparser.py
+++ src/textblockparser.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+
+class TextBlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 class TextBlockParser():
 
     def markdown_to_blocks(markdown):
@@ -8,7 +19,7 @@ class TextBlockParser():
             if line == "" and block == "":
                 continue
             elif line == "" and block != "":
-                blocks.append(block)
+                blocks.append(block.strip())
                 block = ""
             elif block != "":
                 block += "\n" + line
@@ -17,4 +28,31 @@ class TextBlockParser():
         if block != "":
             blocks.append(block)
         return blocks
-            
+    
+    def block_to_block_type(block):
+        if re.match(r"^#{1,6} .*", block) != None:
+            return TextBlockType.HEADING
+        elif re.match(r"^```[^`]*```$", block) != None:
+            return TextBlockType.CODE
+        else:
+            lines = block.split("\n")
+            quote_match, unordered_match, ordered_match = False, False, False
+            order_count = 1
+            for line in lines:
+                if line[0] == ">":
+                    quote_match = True
+                elif line[:2] == "* " or line[0] == "- ":
+                    unordered_match = True
+                elif line[:3] == str(order_count) + ". ":
+                    order_count += 1
+                    ordered_match = True
+                else:
+                    return TextBlockType.PARAGRAPH
+            if quote_match and not unordered_match and not ordered_match:
+                return TextBlockType.QUOTE
+            elif unordered_match and not quote_match and not ordered_match:
+                return TextBlockType.UNORDERED_LIST
+            elif ordered_match and not unordered_match and not quote_match:
+                return TextBlockType.ORDERED_LIST
+            return TextBlockType.PARAGRAPH
+                
