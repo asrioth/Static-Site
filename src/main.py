1164a72334622692e9aa1ca06d6938bda03c2c89
asrioth <liamswords@gmail.com>
2024-12-18 17:30:42 +1300

    modified main to regeneragte public with the contents of static

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from enum import Enum
 from textnode import TextNode, TextType
+import shutil
+import os
+
+def copy_folder_to_public(folder_path):
+    contents = os.listdir(folder_path)
+    for content in contents:
+        file_path = f"{folder_path}{content}"
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, f"public{file_path[6:]}")
+        else:
+            os.mkdir(f"public{file_path[6:]}")
+            copy_folder_to_public(file_path + "/")
 
 def main():
-    textnode = TextNode("hi", TextType.BOLD, "here.com")
-    textnode2 = TextNode("hi", TextType.BOLD, "here.com")
-    print(textnode)
-    print(textnode == textnode2)
-
-    print("a\na\nb\n b".split('\n'))
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    os.mkdir("public")
+    copy_folder_to_public("static/")
 
 if __name__ == "__main__":
     main()
